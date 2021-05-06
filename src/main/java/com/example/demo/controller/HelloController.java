3a6998f71d006612947291b4ccf57384c668036e
cuijiye <cuijiye1993@163.com>
2021-05-06 18:59:21 +0800

    hot-fix first commit

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -21,6 +21,7 @@ public class HelloController {
         model.addAttribute("msg", "<h1>hello,SpringBoot</h1>");
         model.addAttribute("msg2", "<h2>hello,SpringBoot</h2>");
         model.addAttribute("msg3", "<h3>hello,SpringBoot</h3>");
+        model.addAttribute("msg4", "<h4>hello,SpringBoot</h4>");
         model.addAttribute("users", Arrays.asList("zhangsan", "lisi", "wangwu"));
         return "hello";
     }
