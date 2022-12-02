b04778fbfaadf9779cb87142fe4b05cc8d3a9727
崔继业 <cuijiye@huicloud.com>
2022-12-02 16:01:04 +0800

    test2

---
+++ src/main/java/com/example/demo/test/moreThread/Result.java
@@ -0,0 +1,13 @@
+package com.example.demo.test.moreThread;
+
+public class Result {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
